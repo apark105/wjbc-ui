14ee8b7e395c6b7df7907dbef142e86fb1cfbf60
David Yonwook Chung <davidchung@Davids-MBP.fios-router.home>
2019-04-29 19:51:16 -0400

    [setup] implement jsx

--- wjbc-ui-app/src/components/app.jsx
+++ wjbc-ui-app/src/components/app.jsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import '../stylesheets/main.scss';
-import Home from './home';
-import Events from './events';
-import About from './about';
-import Navbar from './navbar';
+import React, { Component } from 'react';
+// import { Route } from 'react-router-dom';
+// import '../stylesheets/main.scss';
+// import Home from './home';
+// import Events from './events';
+// import About from './about';
+// import Navbar from './navbar';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <div className="container">
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route path="/events" component={ Events } />
-        {/* <Route path='/ministries' component={Ministries} />
-                <Route path='/resources' component={Resources} />
-                <Route path='/give' component={Give} />         */}
-      </div>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>demo</div>;
+  }
 }
 
 export default App;
